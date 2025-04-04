70ff2cbe5d5c3202897d182306653f07f88ebc7d
mvntsh <128336751+mvntsh@users.noreply.github.com>
2025-04-04 13:49:45 +0700

    Update Login.php

--- controllers/Login.php
+++ controllers/Login.php
@@ -6,7 +6,8 @@
     {
         function __construct(){
             parent::__construct();
-            //place your model here.
+            $this->load->model("login_m");
+            $this->load->library('session');
         }    
 
         function index(){
@@ -15,6 +16,65 @@
             $this->load->view("login_v");
             $this->load->view("common/footer");
         }
+
+        function userVerification_c(){
+            $data["success"] = false;
+
+			$username = $this->input->post("txtnmUsername");
+            $password = $this->input->post("txtnmPassword");
+
+			$response = $this->login_m->userVerification_m($username,$password);
+
+			if($response){
+				$data["success"] = true;
+				$this->session->set_userdata($response);
+			}
+			echo json_encode($data);
+        }
+
+        function userVerificationapprvr_c(){
+            $data["success"] = false;
+
+			$username = $this->input->post("txtnmUsername");
+            $password = $this->input->post("txtnmPassword");
+
+			$response = $this->login_m->userVerificationapprvr_m($username,$password);
+
+			if($response){
+				$data["success"] = true;
+				$this->session->set_userdata($response);
+			}
+			echo json_encode($data);
+        }
+
+        function userVerificationexe_c(){
+            $data["success"] = false;
+
+			$username = $this->input->post("txtnmUsername");
+            $password = $this->input->post("txtnmPassword");
+
+			$response = $this->login_m->userVerificationexe_m($username,$password);
+
+			if($response){
+				$data["success"] = true;
+				$this->session->set_userdata($response);
+			}
+			echo json_encode($data);
+        }
+
+        function userLogout_c(){
+            $data["success"] = false;
+
+            $username = $this->input->post("txtnmUsername");
+
+            $response = $this->login_m->userLogout_m($username);
+
+            if($response){
+                $data["success"] = true;
+                unset($_SESSION['username']);
+            }
+            echo json_encode($data);
+		}
     }
     
 ?>
